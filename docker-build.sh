ae172a9855ef13ec4afb3cbe2f2a8317ef76154c
localnerve <alex@localnerve.com>
2025-03-17 19:20:07 -0400

    fix: docker build tag name

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker buildx build --tag "localnerve-com" --secret id=jam-build,src=./private/host-env.json . --build-arg UID=`id -u` --build-arg GID=`id -g`
+docker buildx build --tag "jam-build" --secret id=jam-build,src=./private/host-env.json . --build-arg UID=`id -u` --build-arg GID=`id -g`
