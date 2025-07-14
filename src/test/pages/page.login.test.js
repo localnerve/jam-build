510a8cd507ab2842575c773b813fea3ea0558ed0
localnerve <alex@localnerve.com>
2025-07-14 15:59:22 -0400

    first login flow test

---
+++ src/test/pages/page.login.test.js
@@ -0,0 +1,122 @@
+/**
+ * Page login test
+ * 
+ * Copyright (c) 2025 Alex Grant (@localnerve), LocalNerve LLC
+ * Private use for LocalNerve, LLC only. Unlicensed for any other use.
+ */
+import { test, expect } from '../fixtures.js';
+import { acquireAccount } from '../authz.js';
+/*
+import {
+  createTestDataApp,
+  createTestDataUser,
+  deleteTestDataApp,
+  deleteTestDataUser
+} from '../testdata.js';
+*/
+import { startJS, stopJS, createMap, createReport } from '../coverage.js';
+import { initScriptDataUpdate, waitForDataUpdate } from './page.utils.js';
+
+test.describe('login tests', () => {
+  let baseUrl;
+  let map;
+
+  test.beforeAll(async (/* { adminRequest, userRequest } */) => {
+    baseUrl = process.env.BASE_URL;
+    map = createMap();
+    //await createTestDataApp(baseUrl, adminRequest);
+    //await createTestDataUser(baseUrl, userRequest);
+  });
+
+  test.beforeEach(async ({ page }) => {
+    await startJS(page);
+  });
+
+  test.afterEach(async ({ page }) => {
+    await stopJS(page, map);
+  });
+
+  // eslint-disable-next-line no-empty-pattern
+  test.afterAll(async ({ /* adminRequest, userRequest */ }, testInfo) => {
+    await createReport(map, testInfo);
+    //await deleteTestDataApp(baseUrl, adminRequest);
+    //await deleteTestDataUser(baseUrl, userRequest);
+  });
+
+  test('Main login flow', async ({ page }) => {
+    await page.addInitScript(initScriptDataUpdate);
+
+    await page.goto(baseUrl);
+
+    // For debugging
+    await page.addScriptTag({
+      content: 'localStorage.setItem("debug", "data,request,home,login");'
+    });
+
+    // Wait for the app to setup
+    let payload = await waitForDataUpdate(page, {
+      storeType: 'app'
+    });
+    expect(payload.storeType).toEqual('app');
+
+    // Login
+    let logins = await page.getByLabel('Log In').all();
+    let topLogin = logins[1];
+
+    // make sure button is in the expected state
+    await topLogin.locator('.label').waitFor({
+      timeout: 3000
+    });
+    const loginText = await topLogin.innerText();
+    expect(loginText).toEqual('Log In'); // eslint-disable-line  playwright/prefer-web-first-assertions
+
+    // click to login
+    await topLogin.click();
+
+    // @@@ debug it
+    // await new Promise(resolve => setTimeout(resolve, 50000));
+    // @@@
+
+    // wait for navigation to be NOT baseUrl
+    const urlBaseUrl = new URL(baseUrl);
+    const proto = urlBaseUrl.protocol;
+    const rest = baseUrl.split(proto)[1];
+    const reNotBaseUrl = new RegExp(`${proto}(?!${rest})`);
+
+    await page.waitForURL(reNotBaseUrl, {
+      timeout: 5000
+    });
+
+    const loginButton = page.getByText('Log In');
+    const inputUser = page.locator('#authorizer-login-email-or-phone-number');
+    const inputPass = page.locator('#authorizer-login-password');
+  
+    const account = await acquireAccount(test, 'user');
+    await inputUser.fill(account.username);
+    await inputPass.fill(account.password);
+    await loginButton.click();
+
+    // Wait for auth callback
+    await page.waitForURL(`${baseUrl}/?state=**`, {
+      timeout: 5000
+    });
+
+    // Start waiting for user data
+    const promiseForUser = waitForDataUpdate(page, {
+      storeType: 'user'
+    });
+
+    // Verify login state changed
+    logins = await page.getByLabel('Log In').all();
+    topLogin = logins[1];
+    await topLogin.locator('.alt-label').waitFor({
+      timeout: 3000
+    });
+    const logoutText = await topLogin.innerText();
+    expect(logoutText).toEqual('Log Out'); // eslint-disable-line  playwright/prefer-web-first-assertions
+
+    // Finish waiting for user data and verify type received
+    payload = await promiseForUser;
+    expect(payload.storeType).toEqual('user');
+  });
+});
---
+++ src/test/pages/page.utils.js
@@ -0,0 +1,64 @@
+/**
+ * Utility functions for page tests.
+ * 
+ * Copyright (c) 2025 Alex Grant (@localnerve), LocalNerve LLC
+ * Private use for LocalNerve, LLC only. Unlicensed for any other use.
+ */
+
+/**
+ * Browser init script to pre-hook 'database-data-update' and record the last keys sent by storeType.
+ * This prevents missing super fast messages before we get a chance to setup a message handler.
+ */
+export function initScriptDataUpdate () {
+  // console.log('window', window);
+  // console.log('window.navigator', window.navigator);
+
+  if (!window.__databaseDataUpdate) {
+    window.__databaseDataUpdate = Object.create(null);
+  }
+  navigator.serviceWorker?.addEventListener('message', event => {
+    const msgId = event?.data?.meta;
+    const payload = event?.data?.payload;
+    if (msgId === 'database-data-update') {
+      window.__databaseDataUpdate[payload.storeType] = payload.keys;
+    }
+  });
+}
+
+/**
+ * Wait for the database-data-update message.
+ * 
+ * @param {Page} page - The playwright Page fixture
+ * @param {Object} [options] - options object
+ * @param {String} [options.storeType] - The storeType to wait for 'app' or 'user', defaults to 'app'
+ * @param {Number} [options.timeout] - timeout, defaults to 3000
+ * @returns {Promise<Object>} A promise that resolves to the message payload object
+ */
+export function waitForDataUpdate (page, {
+  storeType = 'app',
+  timeout = 3000
+} = {}) {
+  return page.evaluate(([storeType, timeout]) => {
+    let resolve;
+    const waiter = new Promise(res => resolve = res);
+    
+    const keys = window.__databaseDataUpdate[storeType];
+    if (keys) {
+      // console.log('@@@ CAUGHT EARLY INVOCATION');
+      setTimeout(() => resolve({ storeType, keys }), 0);
+    } else {
+      navigator.serviceWorker.addEventListener('message', event => {
+        const msgId = event?.data?.meta;
+        const payload = event?.data?.payload;
+        if (msgId === 'database-data-update') {
+          if (payload.storeType === storeType) {
+            resolve(payload);
+          }
+        }
+      });
+      setTimeout(() => resolve({ storeType: 'timeout' }), timeout);
+    }
+
+    return waiter;
+  }, [storeType, timeout]);
+}
