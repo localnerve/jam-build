72d5f2845f6c14fe582c30399b693942b0328ee0
localnerve <alex@localnerve.com>
2025-08-11 22:46:32 -0400

    AGPLv3

--- src/build/styles.js
+++ src/build/styles.js
@@ -1,8 +1,22 @@
 /**
  * Bundle the main styles.
  * 
- * Copyright (c) 2025 Alex Grant (@localnerve), LocalNerve LLC
- * Private use for LocalNerve, LLC only. Unlicensed for any other use.
+ * Jam-build, a web application practical reference.
+ * Copyright (c) 2025 Alex Grant <info@localnerve.com> (https://www.localnerve.com), LocalNerve LLC
+ *
+ * This file is part of Jam-build.
+ * Jam-build is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ * Jam-build is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with Jam-build.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * Additional terms under GNU AGPL version 3 section 7:
+ * a) The reasonable legal notice of original copyright and author attribution must be preserved
+ *   by including the string: "Copyright (c) 2025 Alex Grant <info@localnerve.com> (https://www.localnerve.com), LocalNerve LLC"
+ *   in this material, copies, or source code of derived works.
  */
 import { Transform } from 'node:stream';
 import path from 'node:path';
