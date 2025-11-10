e122efcd938d7c711f74f5a6926ac7230580a210
localnerve <alex@localnerve.com>
2025-11-10 16:11:15 -0500

    @2.7.1 - fix comment

--- docker-db-restore.sh
+++ docker-db-restore.sh
@@ -4,4 +4,4 @@
 docker compose --env-file .env.dev run --rm hydrate
 
 # Example: Restore from specific backup
-# docker compose run --env-file .env.dev --rm -e BACKUP_FILE=db-20250116-143052.tar.gz hydrate
+# docker compose --env-file .env.dev run --rm -e BACKUP_FILE=db-20250116-143052.tar.gz hydrate
